299bbac6f07a1108e7fd22d4193cebff24a8d779
thane98 <ethan@stonefall.com>
2021-07-11 10:04:28 -0700

    Actual fix for Leo's A001 cutscene model.

--- ignis/core/fe14/step/leo_a001_model_replacement_step.py
+++ ignis/core/fe14/step/leo_a001_model_replacement_step.py
@@ -10,15 +10,34 @@ class LeoA001ModelReplacementStep(RandomizationStep):
 
     def run(self, gd, user_config, dependencies):
         characters = dependencies.characters
+        rom2 = gd.node("rom2").rid
         rom3 = gd.node("rom3").rid
         rom4 = gd.node("rom4").rid
         if replacement_pid := characters.get_replacement("PID_レオン"):
             replacement = characters.to_rid(replacement_pid)
+            replacement_primary_class = gd.rid(replacement, "class_1")
+            replacement_jid = gd.key(replacement_primary_class)
             replacement_aid = gd.string(replacement, "aid")
-            if replacement_aid:
+            if replacement_aid and replacement_jid:
+                body_rid = gd.list_key_to_rid(rom2, "specs", replacement_jid)
                 source_rid = gd.list_key_to_rid(rom3, "specs", replacement_aid)
                 dest_rid = gd.list_key_to_rid(rom4, "specs", "AID_法衣裏返しレオン")
-                if source_rid and dest_rid:
+                if body_rid and source_rid and dest_rid:
                     gd.copy(source_rid, dest_rid, [])
+                    gd.copy(
+                        body_rid,
+                        dest_rid, [
+                            "body_model",
+                            "body_texture",
+                            "outer_clothing_model",
+                            "outer_clothing_texture",
+                            "accessory1_model",
+                            "accessory1_texture",
+                            "accessory2_model",
+                            "accessory2_texture",
+                            "accessory3_model",
+                            "accessory3_texture",
+                        ]
+                    )
                     gd.set_string(dest_rid, "name", "AID_法衣裏返しレオン")
                     gd.set_store_dirty("rom4", True)
