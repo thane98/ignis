429785a23b006d7cf102b63c86dd11c3311d6e85
thane98 <ethan@stonefall.com>
2021-07-11 10:20:45 -0700

    Improve packaging script.

--- package.bat
+++ package.bat
@@ -1,2 +1,6 @@
 pyinstaller -F -n ignis -i ignis.ico ignis\controllers\main.py
-
+copy ignis.ico dist
+copy LICENSE.txt dist
+copy ignis-fe14.yml dist
+xcopy Data dist\Data\ /s /e /y
+xcopy venv\Lib\site-packages\PySide6\plugins\platforms dist\platforms\ /s /e /y
